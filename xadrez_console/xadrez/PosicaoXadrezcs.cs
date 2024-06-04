041562dbbb2da712292de53e0e9b76d0e3f14812
Iuri Trevizani <iurirosa250@gmail.com>
2024-06-04 11:13:05 -0300

    Criando a classe posicao xadrez

---
+++ xadrez_console/xadrez/PosicaoXadrezcs.cs
@@ -0,0 +1,26 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrezcs
+    {
+        public char coluna {  get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrezcs(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
